42d09a16891bad6236c3aeaf9018648842b83db8
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-06-07 06:00:55 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React , { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [book, setBook] = useState('')
+  // const [author, setAuthor] = useState('')
+
+  // const createBook = () => {
+  //   const myObjects = {
+  //     book: book,
+  //     author: author
+  //   }
+  // }
+
+  useEffect(() => {
+    localStorage.setItem('book', JSON.stringify(book));
+  }, [book]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Local storage app</h1>
+     <input type="text" placeholder='book' value={book} onChange={(e) => {setBook(e.target.value)}} /> <br />
+     <button >Submit</button>
     </div>
   );
 }
